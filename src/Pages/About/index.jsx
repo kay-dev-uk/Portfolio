9f7f957fd4b30cd5461e631fd0abfe8df44d1eee
kay-dev-uk <kay.dev.uk@gmail.com>
2023-04-11 12:33:07 +0100

    add about info

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 
 const About = () => {
+
   return (
     <div className='about'>
       <div>
-        <h3>About Me</h3>
-        <h4>Hello and welcome to my About page! I'm a brand-new developer who's just starting out on this exciting journey. I'm constantly pushing myself to grow and expand my knowledge, whether it's through online courses, coding challenges, or collaborating with other developers and I just finished my coding bootcamp with La Fosse. I'm excited to be a part of this vibrant community and can't wait to see where this journey takes me!</h4>
+        <h2>About Me</h2>
+        <p className='about-p'>Hello and welcome to my About page! I'm a brand-new developer who's just starting out on this exciting journey. I'm constantly pushing myself to grow and expand my knowledge, whether it's through online courses, coding challenges, or collaborating with other developers and I just finished my coding bootcamp with La Fosse. I'm excited to be a part of this vibrant community and can't wait to see where this journey takes me!</p>
       </div>
-      <div>
-        <h3>My journey</h3>
-        <p>I had passion for technology ever since young age</p>
+        <div>
+          <h2>My journey</h2>
+        </div>
+      <div className='journey'>
+        <div className='flex-row'>
+          {/* <img src='src/assets/pic.png' alt='image1' /> */}
+          <p>From a young age, I've had a natural passion for technology. As a child, I started interacting with computers at the age of six, and by the time I was ten, I had my very own. While other kids were playing outside, I was tinkering with software and exploring the vast world of the internet. When I was around thirteen or fourteen, I started experimenting with website creation on a platform called uCoz. Looking back, it may not have been true programming, but it sparked something in me.</p>
+        </div>
+        <div className='flex-row-reverse'>
+          {/* <img src='src/assets/pic.png' alt='image2' /> */}
+          <p>After completing university, I rediscovered my passion for technology and began planning how to transition to a career in the field. In 2022, I made the bold decision to take a leap of faith and leave my full-time job. I started by taking Harvard's renowned CS50 course and later discovered La Fosse. I applied and was thrilled to be accepted into their intensive bootcamp program. As I progressed through the program, I found myself fully immersed in the training and couldn't wait to apply my newfound skills in a professional setting.</p>
+        </div>
+        <div className='flex-row'>
+        {/* <img src='src/assets/pic.png' alt='image3' /> */}
+          <p>Now, as a fully trained and passionate developer, I'm eager to apply my skills and knowledge to real-world projects. I thrive in fast-paced environments and love the challenge of solving complex problems. My experience and training have given me the confidence and skills to excel as a developer, and I'm excited to see where this exciting journey takes me.</p>
+        </div>
       </div>
     </div>
   )
 }
 
 export default About
+
