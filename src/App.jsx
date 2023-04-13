5df2d4cda11f802833db5815ecbca79e0912eda0
kay-dev-uk <kay.dev.uk@gmail.com>
2023-04-13 21:54:42 +0100

    dependencies x

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ProjectPage from './Pages/ProjectPage';
 const App = () => {
   return (
     <Routes>
-      <Route path ='/' element={<NavBar />}>
+      <Route path ='/x' element={<NavBar />}>
         <Route index element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/projects' element={<Projects />} />
