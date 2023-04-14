859c9877dfec5bb9313d4c3b674f67e2813e8fee
kay-dev-uk <kay.dev.uk@gmail.com>
2023-04-14 11:03:17 +0100

    depl

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
